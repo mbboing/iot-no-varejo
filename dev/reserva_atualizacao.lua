8f155cc1920ad0945143f2812e09fde8569a865e
mbboing <mbboing@gmail.com>
2017-10-30 15:36:52 -0200

    Avancos dia 30

--- dev/reserva_atualizacao.lua
+++ dev/reserva_atualizacao.lua
@@ -3,6 +3,7 @@ local updatemanager = {};
 local versao_local, versao_atual;
 local arquivos = {};
 local qnt_arquivos_baixados = 0;
+local wget_retries = 0;
 local wgetTimeout = tmr.create();
 
 local function update_files_done() print("Sem callback da atualizacao de arquivos") end;
@@ -40,44 +41,66 @@ local function wget(endereco, arquivo, saida, callback, porta)
 	local file_size = 0;
     local total_size = 0;
     local is_first_package = true;
+	local receiving_flag = false;
+   
     print(endereco, caminho, arquivo, saida, porta);
-	--NO TIMEOUT FAZER UM RETORNO DE ERRO PARA TENTAR NOVAMENTE (COM UM CONTADOR DE RETRIES) E FECHAR A CONEXAO PARA NÂO TER RECIEVE
-	wgetTimeout:alarm(10000, tmr.ALARM_SINGLE, cancel_update);
+	wgetTimeout:alarm(5000, tmr.ALARM_SINGLE, function()
+		receiving_flag = false;
+		s:close();
+		callback(0);
+	end);
     s=net.createConnection(net.TCP, 0);
     s:on("receive", function(sck, c)
-        if(is_first_package) then
-            --Pegando tamanho do arquivo
-            if string.match(c,"Length: %d+") ~= nil then
-			    total_size = tonumber(string.sub(string.match(c,'Length: %d+'),9));
-            end
-            --Cortando o cabecalho 
-            local beginning = string.find(c,"\r\n\r\n");
-            if beginning ~= nil then
-                print(beginning);
-                c = string.sub(c,beginning+4);
-                is_first_package = false;
-            end;
-        end;
-        
-		table.insert(file_content, c);
-        
-        file_size = file_size + string.len(c);
-        print("Tamanho lido: ", file_size .. '/' .. total_size);
-        if file_size >= total_size then
-			wgetTimeout:stop()
-			--Cria o arquivo com o conteudo lido
-    		file.open(saida, "w");
-			for _,i in pairs(file_content) do
-				file.write(i);
-			end
-            file.close();
-            callback(total_size);
-        end
+		if receiving_flag == true then
+		    if(is_first_package) then
+		        --Pegando tamanho do arquivo
+		        if string.match(c,"Length: %d+") ~= nil then
+					total_size = tonumber(string.sub(string.match(c,'Length: %d+'),9));
+		        end
+		        --Cortando o cabecalho 
+		        local beginning = string.find(c,"\r\n\r\n");
+		        if beginning ~= nil then
+		            print(beginning);
+		            c = string.sub(c,beginning+4);
+		            is_first_package = false;
+		        end;
+		    end;
+		    
+			table.insert(file_content, c);
+		    
+		    file_size = file_size + string.len(c);
+		    print("Tamanho lido: ", file_size .. '/' .. total_size);
+			if file_size > 5000 then
+				--Escreve no arquivo o conteudo lido
+                --REMOVER ESSA PARTE QUANDO RESOLVER O WGET DO RESERVA_REDE
+				file.open(saida, "w");
+				for k,i in pairs(file_content) do
+					file.write(i);
+					table.remove(file_content, k);
+				end
+		        file.close();
+		    end
+		    if file_size >= total_size then
+				wgetTimeout:stop()
+				--Cria o arquivo com o conteudo lido
+				file.open(saida, "w");
+				for _,i in pairs(file_content) do
+					file.write(i);
+				end
+		        file.close();
+		        callback(total_size);
+		    end
+		end
     end )
-    s:on("disconnection", function() callback(0); end)
+    s:on("disconnection", function() 
+		print("Disconnected");
+		receiving_flag = false;
+		callback(0);
+	end)
 	s:on("connection", function()
 		print("Conectou\n")
-		s:send("GET "..caminho.." HTTPS/1.1\r\n"..
+		receiving_flag = true;
+		s:send("GET "..caminho.." HTTP/1.1\r\n"..
 		       "Host: "..endereco.."\r\n"..
 		       "Connection: keep-alive\r\n"..
 		       "User-Agent: uPNP/1.0\r\n"..
@@ -92,12 +115,13 @@ local function wget(endereco, arquivo, saida, callback, porta)
 end
 
 local function update_next_file(last_file_size)
-	qnt_arquivos_baixados = qnt_arquivos_baixados + 1;
 
     print("Tamanho baixado: ", last_file_size);
-    print("Tamanho correto: ", versao_atual[arquivos[qnt_arquivos_baixados]]);
+    print("Tamanho correto: ", versao_atual[arquivos[qnt_arquivos_baixados + 1]]);
     
-	if last_file_size == versao_atual[arquivos[qnt_arquivos_baixados]] then
+	if last_file_size >= versao_atual[arquivos[qnt_arquivos_baixados + 1]] then
+		qnt_arquivos_baixados = qnt_arquivos_baixados + 1;
+		wget_retries = 0;
 		if qnt_arquivos_baixados < #arquivos then
 
 			local file_name = arquivos[qnt_arquivos_baixados+1];
@@ -122,31 +146,40 @@ local function update_next_file(last_file_size)
     		reset();
 		end
 	else
-		cancel_update();
+		wget_retries = wget_retries + 1;
+		if wget_retries < 5 then
+			local file_name = arquivos[qnt_arquivos_baixados+1];
+			wget("mbboing.github.io/iot-no-varejo/", file_name, string.gsub(file_name,".lua","_temp.lua"), update_next_file);
+		else
+			cancel_update();
+		end;
 	end
 
 end
 
-local function check_version()
-	versao_local = dofile("versao.lua");
-	versao_atual = dofile("versao_temp.lua");
+local function check_version(file_size)
+	if file_size ~= 0 then
+		versao_local = dofile("versao.lua");
+		versao_atual = dofile("versao_temp.lua");
 
-    print("Checando versoes");
-	if compara_datas(versao_atual.data, versao_local.data) then
-        print("Versao desatualizada");
-		-- Fazer a atualizacao dos arquivos
-		for k,_ in pairs(versao_atual) do
-			if k ~= "data" then
-				table.insert(arquivos, k);
+		print("Checando versoes");
+		if compara_datas(versao_atual.data, versao_local.data) then
+		    print("Versao desatualizada");
+			-- Fazer a atualizacao dos arquivos
+			for k,_ in pairs(versao_atual) do
+				if k ~= "data" then
+					table.insert(arquivos, k);
+				end
 			end
+			local file_name = arquivos[qnt_arquivos_baixados+1];
+			wget("mbboing.github.io/iot-no-varejo/", file_name, string.gsub(file_name,"%.","_temp."), update_next_file);
+		else
+		    print("Versão atual");
+		    cancel_update();
 		end
-		local file_name = arquivos[qnt_arquivos_baixados+1];
-		wget("mbboing.github.io/iot-no-varejo/", file_name, string.gsub(file_name,"%.","_temp."), update_next_file);
-    else
-        print("Versão atual");
-        cancel_update();
+	else
+		cancel_update();
 	end
-
 end
 
 function updatemanager.update(updatefiles_cb)
---
+++ dev/reserva_wifi_config.lua
@@ -0,0 +1,9 @@
+local config = {};
+
+config.token = "abc123";
+
+--Inserir id e senha do wifi
+config.ssid = ;
+config.pwd = ;
+
+return config;
