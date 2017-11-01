3ba1f0e8cdcf70b9252d791456da52307746f6af
mbboing <mbboing@gmail.com>
2017-11-01 12:57:50 -0200

    Testes do dia 1/11

--- docs/ReservaFiles/reserva_config.lua
+++ docs/ReservaFiles/reserva_config.lua
@@ -5,7 +5,8 @@ config.bluetooth_period = 15;
 --Periodo para fazer upload dos arquivos. A unidade é o bluetooth_period
 config.upload_period = 4;
 --De quanto em quanto tempo verifica se há atualização, em minutos
-config.update_period = 2;
+--Sugere-se deixar 1440 (24h) para a atualização ser feita sempre de madrugada, pois a contagem comeca na primeira meia noite do mês
+config.update_period = 1440;
 --Diretorio dos arquivos de atualizacao remota
 config.update_addr = "mbboing.github.io/iot-no-varejo/ReservaFiles/";
 
--- docs/ReservaFiles/versao.lua
+++ docs/ReservaFiles/versao.lua
@@ -3,16 +3,16 @@
 local versao = {
 
 --["data"] = "AAAA:MM:DD:HH:mm"
-["data"] = "2017:11:01:11:40",
+["data"] = "2017:11:01:12:54",
 
 --["nome_do_arquivo"] = tamanho do arquido em bytes
-["reserva_config.lua"] = 458,
 ["reserva_rede.lua"] = 4232,
+["reserva_atualizacao.lua"] = 6413,
+["reserva_config.lua"] = 597,
 ["reserva_beacons.lua"] = 5681,
+["init.lua"] = 116,
 ["reserva_submit.lua"] = 4750,
 ["reserva_main.lua"] = 3945,
-["init.lua"] = 116,
-["reserva_atualizacao.lua"] = 6413,
 
 }
 
