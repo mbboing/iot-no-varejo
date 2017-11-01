ec9adc7538ab9e8d841fd3acd3c6e95b417abd77
mbboing <mbboing@gmail.com>
2017-11-01 09:12:00 -0200

    Testes do dia 1/11

--- docs/versao.lua
+++ docs/versao.lua
@@ -4,10 +4,10 @@ local versao = {
 ["data"] = "2017:10:31:13:58",
 
 --["nome_do_arquivo"] = tamanho do arquido em bytes
-["reserva_atualizacao.lua"] = 6159,
+["reserva_atualizacao.lua"] = 6413,
 ["reserva_beacons.lua"] = 5681,
-["reserva_config.lua"] = 341,
-["reserva_main.lua"] = 4005,
+["reserva_config.lua"] = 445,
+["reserva_main.lua"] = 3945,
 ["reserva_rede.lua"] = 4232,
 ["reserva_submit.lua"] = 4749
 
